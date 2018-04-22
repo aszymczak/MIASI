e7daae3389e34ae78ee377ff35e80db8148101cb
Andrzej Szymczak-Łysiak <a.szymczak-lysiak@student.po.edu.pl>
2018-04-22 14:41:43 +0200

    Działający program wież Hanoi napisany w Visual Basic

--- WieżaHonoiVB/Program.vb
+++ WieżaHonoiVB/Program.vb
@@ -2,6 +2,14 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        MoveTowerDisk(4, "A", "B", "C")
+    End Sub
+
+    Sub MoveTowerDisk(ByVal intDisks As Integer, ByVal strFromTower As String, ByVal strToTower As String, ByVal strViaTower As String)
+        If intDisks > 0 Then
+            MoveTowerDisk(intDisks - 1, strFromTower, strViaTower, strToTower)
+            Console.WriteLine("Move disk {0} from {1} to {2}", intDisks, strFromTower, strToTower)
+            MoveTowerDisk(intDisks - 1, strViaTower, strToTower, strFromTower)
+        End If    
     End Sub
 End Module
