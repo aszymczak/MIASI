a09fe1f35364347b2fe2cc8634f464b671e7b937
Andrzej Szymczak-Łysiak <a.szymczak-lysiak@student.po.edu.pl>
2018-04-29 16:04:46 +0200

    Poprawienie programów C#, VB - dodanie wyliczenie czasu

--- WieżaHanoiCSharp/Program.cs
+++ WieżaHanoiCSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace WieżaHanoiCSharp
 {
@@ -7,7 +8,12 @@ namespace WieżaHanoiCSharp
         static void Main(string[] args)
         {
             Program p1 = new Program();
+            Stopwatch MyTimer = new Stopwatch();
+
+            MyTimer.Start();
             p1.MoveTowerDisk(4, "A", "B", "C");
+            MyTimer.Stop();
+            Console.WriteLine("Hanoi Tower solved in {0}, for 4 disk", MyTimer.Elapsed);
         }
 
         public void MoveTowerDisk(int intDisks, string strFromTower, string strToTower, string strViaTower)
@@ -15,7 +21,7 @@ namespace WieżaHanoiCSharp
             if (intDisks > 0)
             {
                 MoveTowerDisk(intDisks - 1, strFromTower, strViaTower, strToTower);
-                Console.WriteLine("Move disk {0} from {1} to {2}", intDisks, strFromTower, strToTower);
+//                Console.WriteLine("Move disk {0} from {1} to {2}", intDisks, strFromTower, strToTower);
                 MoveTowerDisk(intDisks - 1, strViaTower, strToTower, strFromTower);
             }
         }
--- WieżaHonoiVB/Program.vb
+++ WieżaHonoiVB/Program.vb
@@ -2,13 +2,17 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+
+        Dim MyTimer = Stopwatch.StartNew()
         MoveTowerDisk(4, "A", "B", "C")
+        MyTimer.Stop()
+        Console.WriteLine("Hanoi Tower solved in {0}, for 4 disk", MyTimer.Elapsed)
     End Sub
 
     Sub MoveTowerDisk(ByVal intDisks As Integer, ByVal strFromTower As String, ByVal strToTower As String, ByVal strViaTower As String)
         If intDisks > 0 Then
             MoveTowerDisk(intDisks - 1, strFromTower, strViaTower, strToTower)
-            Console.WriteLine("Move disk {0} from {1} to {2}", intDisks, strFromTower, strToTower)
+'            Console.WriteLine("Move disk {0} from {1} to {2}", intDisks, strFromTower, strToTower)
             MoveTowerDisk(intDisks - 1, strViaTower, strToTower, strFromTower)
         End If    
     End Sub
