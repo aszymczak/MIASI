5b88e692767381d625db11d72c8b34cdd482979d
Andrzej Szymczak-Łysiak <a.szymczak-lysiak@student.po.edu.pl>
2018-04-29 12:10:32 +0200

    Działający program wież Hanoi napisany w C#

---
+++ WieżaHanoiCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WieżaHanoiCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+            p1.MoveTowerDisk(4, "A", "B", "C");
+        }
+
+        public void MoveTowerDisk(int intDisks, string strFromTower, string strToTower, string strViaTower)
+        {
+            if (intDisks > 0)
+            {
+                MoveTowerDisk(intDisks - 1, strFromTower, strViaTower, strToTower);
+                Console.WriteLine("Move disk {0} from {1} to {2}", intDisks, strFromTower, strToTower);
+                MoveTowerDisk(intDisks - 1, strViaTower, strToTower, strFromTower);
+            }
+        }
+    }
+}
