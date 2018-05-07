2db82e9e37d1e5a04e0acbfa7b264d68df7e0328
Andrzej Szymczak-Łysiak <a.szymczak-lysiak@student.po.edu.pl>
2018-05-07 12:34:35 +0200

    Add ReadLine to VB, C# programs, and move count, to count how many times move disk

--- WieżaHanoiCSharp/Program.cs
+++ WieżaHanoiCSharp/Program.cs
@@ -5,21 +5,28 @@ namespace WieżaHanoiCSharp
 {
     class Program
     {
+        private int intMoves = 0;
         static void Main(string[] args)
         {
+            Console.Clear();
             Program p1 = new Program();
             Stopwatch MyTimer = new Stopwatch();
 
+            int intDisks;
+            Int32.TryParse(Console.ReadLine(), out intDisks);
+
             MyTimer.Start();
-            p1.MoveTowerDisk(4, "A", "B", "C");
+            p1.MoveTowerDisk(intDisks, "A", "B", "C");
             MyTimer.Stop();
-            Console.WriteLine("Hanoi Tower solved in {0}, for 4 disk", MyTimer.Elapsed);
+
+            Console.WriteLine("Hanoi Tower solved in {0}, for {1} disks | Move count: {2}.", MyTimer.Elapsed, intDisks, p1.intMoves);
         }
 
         public void MoveTowerDisk(int intDisks, string strFromTower, string strToTower, string strViaTower)
         {
             if (intDisks > 0)
             {
+                intMoves += 1;
                 MoveTowerDisk(intDisks - 1, strFromTower, strViaTower, strToTower);
 //                Console.WriteLine("Move disk {0} from {1} to {2}", intDisks, strFromTower, strToTower);
                 MoveTowerDisk(intDisks - 1, strViaTower, strToTower, strFromTower);
--- WieżaHonoiVB/Program.vb
+++ WieżaHonoiVB/Program.vb
@@ -1,16 +1,27 @@
 Imports System
 
 Module Program
+    Private intMoves = 0
+
     Sub Main(args As String())
+        
+        Dim intDisks As Integer
+        intDisks = 0
+
+        Console.Clear()
+
+        Integer.TryParse(Console.ReadLine(), intDisks)
 
         Dim MyTimer = Stopwatch.StartNew()
-        MoveTowerDisk(4, "A", "B", "C")
+        MoveTowerDisk(intDisks, "A", "B", "C")
         MyTimer.Stop()
-        Console.WriteLine("Hanoi Tower solved in {0}, for 4 disk", MyTimer.Elapsed)
+
+        Console.WriteLine("Hanoi Tower solved in {0}, for {1} disks | Move count: {2}.", MyTimer.Elapsed, intDisks, intMoves)
     End Sub
 
     Sub MoveTowerDisk(ByVal intDisks As Integer, ByVal strFromTower As String, ByVal strToTower As String, ByVal strViaTower As String)
         If intDisks > 0 Then
+            intMoves = intMoves + 1
             MoveTowerDisk(intDisks - 1, strFromTower, strViaTower, strToTower)
 '            Console.WriteLine("Move disk {0} from {1} to {2}", intDisks, strFromTower, strToTower)
             MoveTowerDisk(intDisks - 1, strViaTower, strToTower, strFromTower)
